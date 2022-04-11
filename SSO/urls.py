8d94ef268beb289e852a08c4f3b0ec62ec5322ea
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-04-11 20:54:59 +0100

    route dashboard

--- SSO/urls.py
+++ SSO/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dashboard', views.dashboard, name="dashboard"),
     path('accounts/', include('allauth.urls')),
 ]
