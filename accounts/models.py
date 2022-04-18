1cbdd82cae7b31705f2d7f4b3c3f7b70fdedd723
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-04-18 23:35:39 +0100

    add model class

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class StellarResource(models.Model):
+    user = models.OneToOneField(User, null = True, blank = True, on_delete = models.CASCADE)
+    acc_ID = models.CharField(max_length = 250, blank=True)
+    secretkey = models.CharField(max_length = 250, blank=True)
+    has_account = models.BooleanField(default= False)
+    date_created = models.DateTimeField(auto_now_add=True, null = True)
+
+    def __str__(self):
+        return f"{self.user.username.upper()} - {self.user.email}"
