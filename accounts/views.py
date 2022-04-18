5140ab525d6ba1f9414f52196eb20186e14711bd
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-04-18 23:41:01 +0100

    add fetch home view

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from SSO.settings import server_url, source_secret_key, PAYSTACK_PUBLIC_KEY, APP_CHARGES
+
+# cbv method
+from django.views import View
+
+# SSO
+from .models import *
+from .forms import *
+from .stellar import Stellar
+
 
 # Create your views here.
 
-@login_required
-def dashboard(request):
-    return render(request, "dashboard/index.html", {})
+class Home(View, Stellar):
+    template_name = 'dashboard/index.html'
+
+    def get_User_Stellar_Resource(self, request):
+        return StellarResource.objects.get_or_create(user=request.user)
+
+    @method_decorator(login_required)
+    def get(self, request):
+        stellarResource, _ = self.get_User_Stellar_Resource(request)
+        has_wallet = True if stellarResource.has_account else False
+
+        if has_wallet:
+            return redirect('dashboard')
+        return render(request, self.template_name, {})
+    
