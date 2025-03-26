ad8dde5ae135aba2abee731409873ec7ddc19e1d
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-03-26 17:59:43 +0100

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,8 +2,8 @@
 set -o errexit  
 
 # Install system dependencies
-apt-get update && apt-get install -y python3-dev libpq-dev gcc build-essential
+apt-get update && apt-get install -y python3.11-dev libpq-dev gcc build-essential
 
 # Upgrade pip & install dependencies
 pip install --upgrade pip  
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
