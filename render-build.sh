7e13c43c0164cfe0cb83809f1efd66a8c7101a7f
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-03-26 18:34:15 +0100

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit
 
-# Install required system packages
-apt-get update && apt-get install -y build-essential python3-dev libssl-dev libffi-dev python3-pip
+# Install system dependencies
+apt-get update && apt-get install -y \
+    python3.11 \
+    python3.11-dev \
+    python3-pip \
+    python3-venv \
+    build-essential \
+    libpq-dev \
+    libssl-dev \
+    libffi-dev \
+    cargo \
+    gcc \
+    g++
+
+# Upgrade pip
+pip install --upgrade pip
 
 # Install Python dependencies
-pip install --upgrade pip
 pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
